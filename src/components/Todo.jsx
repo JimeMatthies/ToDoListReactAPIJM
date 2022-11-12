48f70cfbb28e5ee8a8f75c6baed93770420f545f
Jime Matthies <JimeMatthies@gmail.com>
2022-11-12 02:24:35 -0300

    To Do List React API JM

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Todo = () => {
 
     const [inputValue, setInputValue] = useState("");
     const [tasks, setTasks] = useState([]);
     const [disable, setDisable] = useState(false);
+    const URL_API = "https://assets.breatheco.de/apis/fake/todos/user/jimematthies";
+
+    useEffect(() => {
+        getTasks(URL_API)
+        return () => { }
+    }, []);
+
+    const getTasks = (url, options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }) => {
+        fetch(url, options)
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setTasks(data);
+            }).catch(error => {
+                console.log(error);
+            });
+    };
+
+    const putTasks = (url, options = {
+        method: 'PUT',
+        body: JSON.stringify(tasks),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }) => {
+        fetch(url, options)
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+            }).catch(error => {
+                console.log(error);
+            });
+    };
 
     const handleChange = e => {
         setInputValue(e.target.value);
@@ -12,9 +56,64 @@ const Todo = () => {
 
     const handleSubmit = e => {
         e.preventDefault();
-        if (inputValue === "") return;
-        setTasks([...tasks, inputValue]);
-        setInputValue("");
+        if (inputValue.trim() != "" && inputValue != null) {
+            setTasks([...tasks, { label: inputValue, done: true }]);
+            putTasks(URL_API);
+            setInputValue.setTasks;
+        };
+    };
+
+    const deleteItem = key => {
+        const newList = tasks.filter(task => {
+            return task.label != key;
+        });
+        setTasks(newList);
+        fetch(URL_API,
+            {
+                method: "PUT",
+                body: JSON.stringify(newList),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+            }).catch(error => {
+                console.log(error);
+            });
+    };
+
+    const deleteAll = () => {
+        setTasks([
+            {
+                label: "",
+                done: false
+            }
+        ]);
+        fetch(
+            "https://assets.breatheco.de/apis/fake/todos/user/jimematthies",
+            {
+                method: "PUT",
+                body: JSON.stringify(tasks),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+            }).catch(error => {
+                console.log(error);
+            });
     };
 
     return (
@@ -27,21 +126,19 @@ const Todo = () => {
             <div className="list-container d-flex justify-content-start">
                 <ul>
                     {tasks.map((task, index) => {
-                        return <li className="d-flex justify-content-between aling-text-center" key={index} onMouseOver={(e) => {
-                            setDisable(true)
-                        }} onMouseOut={(e) => {
-                            setDisable(false)
-                        }}>{task}<a key={index} className={disable ? "trash" : "trash disable"} onClick={(e) => {
-                            setTasks(tasks.filter((task, id) => {
-                                return index !== id
-                            }))
-                        }}>X</a></li>
+                        if (task.label != "") {
+                            return <li className="task d-flex justify-content-between aling-text-center" key={index} onMouseOver={(e) => {
+                                setDisable(true)
+                            }} onMouseOut={(e) => {
+                                setDisable(false)
+                            }}>{task.label}<a key={index} className={disable ? "trash" : "trash disable"} onClick={() => deleteItem(task.label)}>X</a></li>
+                        }
                     })}
                     <div className="counter">{tasks.length === 0 ? "No tasks, add a task" : tasks.length + " tasks left"}</div>
                 </ul>
             </div>
             <div className="delete-btn d-flex justify-content-center">
-                <button>Remove All</button>
+                <button className="delete-btn" onClick={() => { deleteAll() }}>Remove All</button>
             </div>
         </div>
     );
